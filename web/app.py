adf1ce67bddc455731dbf8fb08a237142a93d7ac
gabapppp <khiempham201@gmail.com>
2023-12-09 09:35:46 +0700

    Update

--- web/app.py
+++ web/app.py
@@ -3,11 +3,29 @@ from flask_socketio import SocketIO
 from random import random
 from threading import Lock
 from datetime import datetime
-import sqlite3
+from redis import Redis
+
+redis = Redis(host='localhost', port=6379, decode_responses=True)
+"""
+Background Thread
+"""
+thread = None
+thread_lock = Lock()
+
+
+dummy_sensor_value = 0
+co_sensor_value = 0
+temp_sensor_value = 0
+humi_sensor_value = 0
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'gabap'
+app.config['SECRET_KEY'] = 'ZUCGiYmt_H3~ZGJ'
 socketio = SocketIO(app, cors_allowed_origins='*')
+
+
+"""
+Get current date time
+"""
 def get_current_datetime():
     now = datetime.now()
     return now.strftime("%m/%d/%Y %H:%M:%S")
@@ -15,26 +33,33 @@ def get_current_datetime():
 """
 Generate random sequence of dummy sensor values and send it to our clients
 """
+
 def background_thread():
     print("Generating random sensor values")
     while True:
         dummy_sensor_value = round(random() * 100, 3)
-        socketio.emit('updateSensorData', {'value': dummy_sensor_value, "date": get_current_datetime()})
+        co_sensor_value = round(random() * 100, 3)
+        temp_sensor_value = round(random() * 100, 3)
+        humi_sensor_value = round(random() * 100, 3)
+        print(humi_sensor_value, temp_sensor_value, co_sensor_value, dummy_sensor_value)
+        #socketio.emit('updateSensorData', {'pm25': dummy_sensor_value,'co': co_sensor_value,
+        #'temp': temp_sensor_value,'humi': humi_sensor_value, "date": get_current_datetime()
+        #})
+        socketio.emit('updateSensorData', {"value": dummy_sensor_value, "date": get_current_datetime()})
         socketio.sleep(1)
-
-
-
 
 @app.route("/")
 def index():
+    
     return render_template("index.html")
 
 @app.route("/mq135")
-def get_mq135_data():
+def get_mq135_list():
+    
     return Response({"None: None"})
 
 @app.route("/pm25")
-def get_pm25_data():
+def get_pm25_list():
     return Response({})
 
 
---
+++ web/static/js/scripts.js
@@ -0,0 +1,119 @@
+const ctx1 = document.getElementById('chart1').getContext('2d');
+const percent_value = 554;
+const chart = new Chart(ctx1, {
+    type: 'doughnut',
+    data: {
+        labels: ['CO',],
+        datasets: [{
+            label: 'CO',
+            data: [percent_value, 1500 - percent_value],
+            backgroundColor: ['#00baa6', '#ededed']
+        }]
+    },
+    options: {
+
+    }
+});
+
+const ctx2 = document.getElementById('chart2').getContext('2d');
+const percent_value2 = 40;
+const chart2 = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: ['PM2.5',],
+        datasets: [{
+            label: 'PM2.5',
+            data: [percent_value, 200 - percent_value],
+            backgroundColor: ['#f01e2c', '#ededed']
+        }]
+    },
+    options: {}
+});
+
+const ctx3 = document.getElementById('chart3').getContext('2d');
+const percent_value3 = 29;
+const chart3 = new Chart(ctx3, {
+    type: 'doughnut',
+    data: {
+        labels: ['Temperature',],
+        datasets: [{
+            label: 'Temperature',
+            data: [percent_value, 100 - percent_value],
+            backgroundColor: ['#00baa6', '#ededed']
+        }]
+    },
+    options: {
+
+    }
+});
+
+const ctx4 = document.getElementById('chart4').getContext('2d');
+const percent_value4 = 40;
+const chart4 = new Chart(ctx4, {
+    type: 'doughnut',
+    data: {
+        labels: ['Humidity',],
+        datasets: [{
+            label: 'Humidity',
+            data: [percent_value, 200 - percent_value],
+            backgroundColor: ['#f01e2c', '#ededed']
+        }]
+    },
+    options: {}
+});
+
+const ctx5 = document.getElementById("chart5").getContext('2d');
+const chart5 = new Chart(ctx5, {
+    type: 'line',
+    data: {
+        labels: ["Sunday", "Monday", "Tuesday",
+            "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday",
+            "Wednesday", "Thursday", "Friday", "Saturday"],
+        datasets: [{
+            label: 'Temperature',
+            backgroundColor: 'rgba(161, 198, 247, 1)',
+            borderColor: 'rgb(47, 128, 237)',
+            data: [500, 400, 600, 900, 300, 600, 500, 500, 400, 600, 900, 300, 600, 500],
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+
+        }
+    },
+});
+
+
+const ctx6 = document.getElementById("chart6").getContext('2d');
+const chart6 = new Chart(ctx6, {
+    type: 'line',
+    data: {
+        labels: ["Sunday", "Monday", "Tuesday",
+            "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday",
+            "Wednesday", "Thursday", "Friday", "Saturday"],
+        datasets: [{
+            label: 'PPM',
+            backgroundColor: 'rgba(161, 198, 247, 1)',
+            borderColor: 'rgb(47, 128, 237)',
+            data: [500, 400, 600, 900, 300, 600, 500, 500, 400, 600, 900, 300, 600, 500],
+        },
+        {
+            label: 'CO',
+            backgroundColor: 'rgba(161, 128, 47, 1)',
+            borderColor: 'rgb(47, 128, 237)',
+            data: [40, 60, 30, 90, 30, 70, 50, 50, 40, 80, 20, 30, 30, 20],
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+
+        }
+    },
+});
+
